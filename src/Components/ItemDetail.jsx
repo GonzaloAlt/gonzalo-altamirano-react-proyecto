584af47fd63d91b126d9a8362cc8c342d05f6c09
Gonzalo Altamirano <gonza12683@gmail.com>
2022-03-28 17:36:21 -0300

    Update ItemDetail.jsx

--- src/Components/ItemDetail.jsx
+++ src/Components/ItemDetail.jsx
@@ -13,12 +13,8 @@ export const ItemDetail = ({ product }) => {
   const [count, setCount] = useState(1);
   const { addToCart, isProductInCart, addToExistingProd, limitStock } =
     useContext(CartContext);
-  const {
-    favourites,
-    addToFavourites,
-    removeFromFavourites,
-    isProductInFavourites,
-  } = useContext(FavouritesContext);
+  const { addToFavourites, removeFromFavourites, isProductInFavourites } =
+    useContext(FavouritesContext);
 
   const item = {
     id,
@@ -195,7 +191,7 @@ export const ItemDetail = ({ product }) => {
               {isProductInCart(id) && (
                 <Link
                   to={"/cart"}
-                  className="flex ml-auto text-white bg-[bg-red] border-0 py-2 px-4 focus:outline-none hover:bg-[#345434] rounded"
+                  className="flex ml-auto text-white bg-[#014801] border-0 py-2 px-4 focus:outline-none hover:bg-[#345434] rounded"
                 >
                   <button>Ir al carrito</button>
                 </Link>
@@ -206,7 +202,6 @@ export const ItemDetail = ({ product }) => {
                 onClick={() => {
                   toggleFavourites();
                   setFavAnimation();
-                  console.log(favourites);
                 }}
                 className={`rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4`}
               >
